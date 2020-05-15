22c163823e596b24cbe0664ce8a23fdd06799b1f
Gustavs Venters <gustavs.venters@gmail.com>
2020-05-15 15:20:32 +0200

    Add sentry during presentation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler, Injectable } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({
+  dsn: 'http://9458a340ae6d4493bab110f91eeb90e8@localhost:9000/4'
+});
+
+@Injectable()
+export class SentryErrorHandler implements ErrorHandler {
+  constructor() {}
+  handleError(error) {
+    const eventId = Sentry.captureException(error.originalError || error);
+    // Sentry.showReportDialog({ eventId });
+  }
+}
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [{ provide: ErrorHandler, useClass: SentryErrorHandler }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {}
